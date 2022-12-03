bfb66a4251e48cc472fe8348afb2017599551128
bkosm <bartoshmen@gmail.com>
2022-12-03 16:49:46 +0100

    d3p1: done

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,21 @@
+import kotlin.math.roundToInt
+
 object Day03 : DailyRunner<Int, Int> {
-    private fun mapInput(input: List<String>): Collection<Int> {
-        var elf = 1
-        val map = mutableMapOf(elf to 0)
+    val PriorityMap = ((('a'..'z') zip (1..26)) + (('A'..'Z') zip (27..52))).toMap()
 
-        input.forEach {
-            if (it.isBlank()) {
-                elf++
-                map[elf] = 0
-            } else {
-                map[elf] = map[elf]!! + it.toInt()
-            }
-        }
+    fun common(a: String, b: String) = a.toSet().intersect(b.toSet())
 
-        return map.values
+    override fun do1(input: List<String>) = input.sumOf { line ->
+        val aSize = (line.length / 2.0).roundToInt()
+        val bSize = line.length - aSize
+
+        val a = line.dropLast(bSize)
+        val b = line.drop(aSize)
+
+        common(a, b).sumOf { PriorityMap[it]!! }
     }
 
-    override fun do1(input: List<String>) = mapInput(input).max()
-    override fun do2(input: List<String>) = mapInput(input).sortedDescending().slice(0..2).sum()
+    override fun do2(input: List<String>) = 1
 }
 
 fun main() {
