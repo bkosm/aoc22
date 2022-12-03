45c30b7b06ae59efead919d61389e1ddd2f9976c
bkosm <bartoshmen@gmail.com>
2022-12-03 16:32:26 +0100

    d3: setup

---
+++ src/Day03.kt
@@ -0,0 +1,24 @@
+object Day03 : DailyRunner<Int, Int> {
+    private fun mapInput(input: List<String>): Collection<Int> {
+        var elf = 1
+        val map = mutableMapOf(elf to 0)
+
+        input.forEach {
+            if (it.isBlank()) {
+                elf++
+                map[elf] = 0
+            } else {
+                map[elf] = map[elf]!! + it.toInt()
+            }
+        }
+
+        return map.values
+    }
+
+    override fun do1(input: List<String>) = mapInput(input).max()
+    override fun do2(input: List<String>) = mapInput(input).sortedDescending().slice(0..2).sum()
+}
+
+fun main() {
+    Day03.run()
+}
