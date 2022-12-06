02b0096f9865cede896abe5e075e68aba6eb748b
bkosm <bartoshmen@gmail.com>
2022-12-06 18:15:51 +0100

    d6: refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,14 +2,13 @@ fun areAllUnique(chars: Collection<Char>) = chars.run {
     toSet().size == size
 }
 
-fun positionOfSignalOfSize(line: String, signalSize: Int): Int {
-    val indexOfFirstUniqueSet = line.toList().windowed(signalSize, 1).mapIndexed { i, it ->
-        if (areAllUnique(it)) i
-        else null
-    }.filterNotNull().first()
-
-    return indexOfFirstUniqueSet + signalSize
-}
+fun positionOfSignalOfSize(line: String, signalSize: Int) = line
+    .toList()
+    .windowed(signalSize, 1)
+    .mapIndexed { i, it -> if (areAllUnique(it)) i else null }
+    .filterNotNull()
+    .first()
+    .let { it + signalSize }
 
 object Day06 : DailyRunner<List<Int>, List<Int>> {
     override fun do1(input: List<String>, isTest: Boolean) = input.map { line ->
