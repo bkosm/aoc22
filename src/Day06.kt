271ca791f610a14a74a6e809cf561f8986f04fcd
bkosm <bartoshmen@gmail.com>
2022-12-06 18:13:38 +0100

    d6: this was easy c:

---
+++ src/Day06.kt
@@ -0,0 +1,26 @@
+fun areAllUnique(chars: Collection<Char>) = chars.run {
+    toSet().size == size
+}
+
+fun positionOfSignalOfSize(line: String, signalSize: Int): Int {
+    val indexOfFirstUniqueSet = line.toList().windowed(signalSize, 1).mapIndexed { i, it ->
+        if (areAllUnique(it)) i
+        else null
+    }.filterNotNull().first()
+
+    return indexOfFirstUniqueSet + signalSize
+}
+
+object Day06 : DailyRunner<List<Int>, List<Int>> {
+    override fun do1(input: List<String>, isTest: Boolean) = input.map { line ->
+        positionOfSignalOfSize(line, 4)
+    }
+
+    override fun do2(input: List<String>, isTest: Boolean) = input.map { line ->
+        positionOfSignalOfSize(line, 14)
+    }
+}
+
+fun main() {
+    Day06.run()
+}
