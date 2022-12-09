9f939a8f0dd0b67b85808493102a6dfdde714825
bkosm <bartoshmen@gmail.com>
2022-12-09 10:55:02 +0100

    d8: init

---
+++ src/Day08.kt
@@ -0,0 +1,8 @@
+object Day08 : DailyRunner<Int, Int> {
+    override fun do1(input: List<String>, isTest: Boolean): Int = 1
+    override fun do2(input: List<String>, isTest: Boolean): Int = 1
+}
+
+fun main() {
+    Day08.run()
+}
